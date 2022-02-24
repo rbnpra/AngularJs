d12a95009d265d72db073a389c6ae4b9e8f103c5
rabin <rabinp@uw.edu>
2022-02-23 21:40:31 -0800

    object created

---
+++ ts-hello/main.ts
@@ -0,0 +1,65 @@
+// let a: number;
+// let b: boolean;
+// let c: string;
+// let d: any;
+// let e: number[] = [1, 2, 3];
+// let f: any[] = [1, true, 'a', false];
+
+// const ColorRed = 0;
+// const ColorGreen =1;
+// const ColorBlue =2;
+
+// enum Color { Red=0, Green=1, Blue=2, Purple = 3};
+// let backgroundColor = Color.Red;
+
+// let message;
+// message = 'abc';
+// let endsWithC = (<string>message).endsWith('c');
+// let alternativeWay = (message as string).endsWith('c');
+
+// let log = function(message) {
+//     console.log(message);
+// }
+// let doLog = () => {
+//     console.log();
+// }
+// interface Point {
+//     x: number, 
+//     y: number
+// }
+// // let drawPoint = (point: {x: number, y: number}) => { // inline anotation
+//     let drawPoint = (point: Point) => {
+// }
+
+// drawPoint({
+//     x: 1,
+//     y: 2
+// })
+
+class Point {
+    x: number;
+    y: number;
+
+    draw() {
+        console.log('X: ' + this.x + ', Y: ' + this.y);
+
+    }
+
+    getDistance(another: Point) {
+
+    }
+}
+
+let point = new Point();
+point.x =1;
+point.y =2;
+
+point.draw();
+
+
+
+
+
+
+
+
